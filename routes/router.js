b6dca9fa7e3bdb315918486dbababd0b7ae9607f
mpuchan <iputumpu23@gmail.com>
2020-07-15 12:47:25 +0800

    change ini

--- routes/router.js
+++ routes/router.js
@@ -2,9 +2,9 @@ var express = require('express')
 var router = express.Router()
 const auth = require('../middlewares/auth_admin')
 
-router.get('/', auth.isLogin, function (req, res, next) {
-  res.redirect("/admin")
-})
+// router.get('/', auth.isLogin, function (req, res, next) {
+//   res.redirect("/admin")
+// })
 
 /* GET home page. */
 router.get('/admin', auth.isLogin, function (req, res, next) {
